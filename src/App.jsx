ea81187127ee07d32a1be991e51d14e381c013d0
Eissa Purnell <eissamonet@gmail.com>
2025-11-20 09:28:03 -0600

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
-  return (
-    <div>
 
+  const [theme, setTheme] = useState('light')
+
+  return (
+    <div className='dark:bg-black relative'>
+      <Navbar theme={theme} setTheme={setTheme} />
     </div>
   )
 }
