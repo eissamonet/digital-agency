612986d71be10533e0c8f8c5d937e0b05824d198
Eissa Purnell <eissamonet@gmail.com>
2025-11-21 08:51:45 -0600

    commit changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,8 +19,9 @@ const Navbar = ({theme, setTheme}) => {
         </div>
 
         <div>
-           <a href="contact-us">
-            Connect <img src={assets.arrow_icon} alt='' />
+           <a href="contact-us" className='text-sm max-sm:hidden flex items-center gap-2 bg-primary text-white px-6 py-2
+           rounded-full cursor-pointer hover:scale-103 transistion-all'>
+            Connect <img src={assets.arrow_icon} width={14} alt='' />
            </a>
         </div>
     </div>
