ed1eecf6d3768ad7c8ad711f7973e6b57496d884
Eissa Purnell <eissamonet@gmail.com>
2025-11-23 17:36:01 -0600

    commit changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,9 @@ const Hero = () => {
             <p className='text-xs font-medium'>Trusted by 10k people</p>
         </div>
 
-        <h1>Turning imagination into <span className='bg-gradient-to-r from-[#5044E5] to-[#4d8cea] bg-clip-text text-transparent'>digital</span> impact.</h1>
+        <h1 className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl'>Turning imagination into <span className='bg-gradient-to-r from-[#5044E5] to-[#4d8cea] bg-clip-text text-transparent'>digital</span> impact.</h1>
+
+        <p>Creating meaningful connections and turning big ideas into interactive digial experiences.</p>
 
     </div>
   )
