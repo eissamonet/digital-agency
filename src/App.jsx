a313d8fde94001baaba27521460bdbc495414697
Eissa Purnell <eissamonet@gmail.com>
2025-11-22 21:25:00 -0600

    dark + light mode theme added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ThemeToggleBtn from './components/ThemeToggleBtn'
 
 const App = () => {
 
-  const [theme, setTheme] = useState('light')
+  const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
 
   return (
     <div className='dark:bg-black relative'>
