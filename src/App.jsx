4db973142e5ddb31ade731b3cf20523331877f9c
Eissa Purnell <eissamonet@gmail.com>
2025-11-23 17:31:13 -0600

    hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import ThemeToggleBtn from './components/ThemeToggleBtn'
+import Hero from './components/Hero';
 
 const App = () => {
 
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <div className='dark:bg-black relative'>
       <Navbar theme={theme} setTheme={setTheme} />
+      <Hero />
     </div>
   )
 }
