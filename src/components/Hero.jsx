0aaf2086c2160ee6e265910acb24e01d37889024
Eissa Purnell <eissamonet@gmail.com>
2025-11-23 17:40:56 -0600

    hero img added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,11 @@ const Hero = () => {
 
         <h1 className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl'>Turning imagination into <span className='bg-gradient-to-r from-[#5044E5] to-[#4d8cea] bg-clip-text text-transparent'>digital</span> impact.</h1>
 
-        <p>Creating meaningful connections and turning big ideas into interactive digial experiences.</p>
+        <p className='text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-4/5 sm:max-w-lg pb-3'>Creating meaningful connections and turning big ideas into interactive digial experiences.</p>
+
+        <div className='relative'>
+            <img src={assets.hero_img} className='w-full max-w-6xl' alt='' />
+        </div>
 
     </div>
   )
