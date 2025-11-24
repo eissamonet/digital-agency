424fe44ec096998e455bb4c5e77ccec5ed7c17d3
Eissa Purnell <eissamonet@gmail.com>
2025-11-24 09:56:10 -0600

    title component added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -26,8 +26,8 @@ const Services = () => {
         }
     ]
   return (
-    <div>
-
+    <div id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+        <img src={assets.bgImage2} alt='' className='absolute -top-110 -left-70 -z-1 dark:hidden' />
     </div>
   )
 }
