333120a47410c90fe725deefef3e1d8a93c7e16b
Eissa Purnell <eissamonet@gmail.com>
2025-11-24 09:30:33 -0600

    services added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
-import Navbar from './components/Navbar'
-import ThemeToggleBtn from './components/ThemeToggleBtn'
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import TrustedBy from './components/TrustedBy';
 
---
+++ src/components/Services.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import assets from '../assets /assets'
+
+const Services = () => {
+
+    const ServicesData = [
+        {
+          title: 'Advertising',
+          description: "We turn bold ideas into powerful advertising campaigns that captivate audiences and drive results.",
+          icon: assets.ads_icon
+        },
+        {
+          title: 'Content Marketing',
+          description: "We help you execute your plan and deliver content that converts.",
+          icon: assets.marketing_icon
+        },
+        {
+          title: 'Content Writing',
+          description: "Our skilled writers create compelling content that engages your audience and boosts your brand's online presence.",
+          icon: assets.content_icon
+        },
+        {
+          title: 'Social Media Management',
+          description: "We manage your social media platforms to enhance your brand visibility and engage with your audience effectively.",
+          icon: assets.social_icon
+        }
+    ]
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Services
