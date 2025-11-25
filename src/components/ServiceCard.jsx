209748595482a17dbec3eb31c55ff2ae430667f1
Eissa Purnell <eissamonet@gmail.com>
2025-11-24 18:57:51 -0600

    service card component added

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ServiceCard = () => {
+  return (
+    <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100
+    dark:shadow-white/10'>
+
+    </div>
+  )
+}
+
+export default ServiceCard
