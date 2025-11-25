bd2234a84efafeda5e80002e3287ccc32804eeea
Eissa Purnell <eissamonet@gmail.com>
2025-11-25 09:33:06 -0600

    added styling to service card

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -7,8 +7,8 @@ const ServiceCard = ({ service, index }) => {
   return (
     <div
       className="relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100
-    dark:shadow-white/10"
-    >
+    dark:shadow-white/10" onMouseEnter={()=>setVisible(true)} onMouseLeave={()=>setVisible(false)}
+      >
       <div
         className={`pointer-events-none blur-2xl rounded-full bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500
           w-[300px] h-[300px] absolute z-0 transition-opacity duration-500 mix-blend-lighten ${visible ? 'opacity-70' : 'opacity-0'}`}
