66cdafa45607da0fb5b0a624da02a4aabe120f19
Eissa Purnell <eissamonet@gmail.com>
2025-11-24 19:55:09 -0600

    commit changes

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import assets from '../assets /assets'
 import Title from './Title'
+import ServiceCard from './ServiceCard'
 
 const Services = () => {
 
@@ -31,7 +32,7 @@ const Services = () => {
         <img src={assets.bgImage2} alt='' className='absolute -top-110 -left-70 -z-1 dark:hidden' />
     <Title title='How can we help?' desc='From strategy to execution, we craft digital solutions that move your business forward.' />
 
-    <div className='flex flex-col md:grid grid-col-s'>
+    <div className='flex flexcol md:grid grid-cols-2'>
        {ServicesData.map((service, index) => (
         <ServiceCard key={index} service={service} index={index} />
        ))}
