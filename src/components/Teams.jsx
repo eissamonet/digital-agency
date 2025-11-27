d8497fc96bc43db99bac34d22c4e6479230ed4ea
Eissa Purnell <eissamonet@gmail.com>
2025-11-27 16:20:17 -0600

    teams component added

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Title from './Title'
+import { teamData } from '../assets /assets'
 
 const Teams = () => {
   return (
@@ -7,6 +8,14 @@ const Teams = () => {
     dark:text-white'>
        <Title title='Meet Our Team' desc='A passionate team of digital experts dedicated to your brands success.'/>
 
+       <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5'>
+        {teamData.map((team, index)=>(
+          <div key={index} className='flex max-sm:flex-col items-center gap-5 p-4 rounded-xl border border-gray-100
+          dark:border-gray-700 bg-white dark:bg-gray-900 shadow-xl shadow-gray-100 dark:shadow-white/5 hover:scale-103 transition-all duratin-400'>
+
+          </div>
+        ))}
+       </div>
     </div>
   )
 }
