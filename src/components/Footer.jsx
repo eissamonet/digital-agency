c232fe09e63dbf8832826bc6d8f5422235012c30
Eissa Purnell <eissamonet@gmail.com>
2025-11-28 10:47:37 -0600

    footer comp added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-slate-50 dark:bg-gray-900 pt-10 sm:pt-10 mt-20 sm:mt-40 px-4
+    sm:px-10 lg:px-24 xl:px-40'>
+      {/*top footer*/}
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
