b4d77eb85a54d4d401e643f6c6b8ff4810ebbd96
Eissa Purnell <eissamonet@gmail.com>
2025-11-28 11:00:47 -0600

    commit footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import assets from '../assets /assets'
 
 const Footer = () => {
   return (
     <div className='bg-slate-50 dark:bg-gray-900 pt-10 sm:pt-10 mt-20 sm:mt-40 px-4
     sm:px-10 lg:px-24 xl:px-40'>
       {/*top footer*/}
-      <div>
-        <div></div>
+      <div className='flex justify-between lg:items-center max-lg:flex-col gap-10'>
+        <div>
+           <img src={assets.logo} className='w-32 sm:w-44' alt="" />
+           <p className='max-w-md'>From strategy to execution, we craft digital solutions that move your business forward.</p>
+
+           <ul>
+            <li><a className='hover:text-primary' href='#hero'>Home</a></li>
+           </ul>
+        </div>
         <div></div>
       </div>
     </div>
