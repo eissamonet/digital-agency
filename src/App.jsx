22357c42f8db6858f18b27e4bd8f0cc0d3dfa737
Eissa Purnell <eissamonet@gmail.com>
2025-11-29 16:24:05 -0600

    custom cursor added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import TrustedBy from './components/TrustedBy';
@@ -10,6 +10,9 @@ import {Toaster} from 'react-hot-toast'
 import Footer from './components/Footer';
 
 const App = () => {
+
+  const dotRef = useRef(null);
+  const outlineRef = useRef(null);
 
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
 
@@ -24,6 +27,13 @@ const App = () => {
       <Teams />
       <ContactUs />
       <Footer theme={theme} />
+
+     {/* custom cursor ring */}
+     <div ref={outlineRef} className='fix top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]'></div>
+
+     {/* custom cursor dot */}
+     <div ref={dotRef} className='fixed top-0 left-0 h-3 w-3 rounded-full bg-primary pointer-events-none z-[9999]'></div>
+
     </div>
   )
 }
