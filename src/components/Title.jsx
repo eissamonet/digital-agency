2646e96310d8de64d71ff8486b2e16f8a353ca96
Eissa Purnell <eissamonet@gmail.com>
2025-12-01 09:35:52 -0600

    commit changes

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { motion} from "motion/react"
 
 const Title = ({title, desc}) => {
   return (
     <>
-     <h2 className='text-3xl sm:text-5xl font-medium'>{title}</h2>
-     <p className='max-w-lg text-center text-gray-500 dark:text-white/75 mb-6'>{desc}</p>
+     <motion.h2 className='text-3xl sm:text-5xl font-medium'>{title}</motion.h2>
+     <motion.p className='max-w-lg text-center text-gray-500 dark:text-white/75 mb-6'>{desc}</motion.p>
     </>
   )
 }
